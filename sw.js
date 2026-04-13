7417b15ea71d6c74c2e116fc1f4d9a58cf33e0f7
qnt-11 <nd4zmoe@gmail.com>
2026-04-13 21:10:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,22 @@
-const CACHE_NAME = 'barla-store-v17.9-premium';
+const CACHE_NAME = "barla-store-v17.9";
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
-  'https://unpkg.com/html5-qrcode'
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-// Install Service Worker dan simpan file ke memori (cache)
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Ambil dari cache dulu, kalau tidak ada baru ambil dari internet
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Hapus cache versi lama jika ada pembaruan
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });

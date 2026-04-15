becb80e4257d6a79d69b8399a71dfbfc8c5bd362
Barla Fam <nd4zmoe@gmail.com>
2026-04-15 21:03:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,37 @@
-const CACHE_NAME = 'barla-kasir-v18.0';
-
-// File utama yang disimpan ke memori HP untuk akses offline
+const CACHE_NAME = 'barla-store-v18.0';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
-// Proses Instalasi & Menyimpan Cache
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-  // Memaksa service worker baru untuk langsung aktif
-  self.skipWaiting();
-});
-
-// Proses Aktivasi & Menghapus Cache Versi Lama (Biar gak nyangkut)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Menghapus cache lama:', cacheName);
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
-  self.clients.claim();
+    );
+    self.skipWaiting();
 });
 
-// Proses Fetch (Jaringan Pintar: Coba internet dulu, kalau gagal pakai cache/offline)
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
